fed6f912117e9ba759a499d05eb76dfdce2e3274
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-17 14:08:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Descobrir a hipotenusa de um triângulo
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("Introduza o valor de A: ");
+         double a = Convert.ToDouble(Console.ReadLine());
+
+         Console.Write("Introduza o valor de B: ");
+         double b = Convert.ToDouble(Console.ReadLine());
+
+         double c = Math.Sqrt((a*a) + (b*b));
+         Console.WriteLine("A hipotnusa é: " + c);
+
+    }
+}
